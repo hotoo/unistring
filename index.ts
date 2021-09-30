7514e57681108d1bca1f4abc009d77da3085301b
闲 耘 <hotoo.cn@gmail.com>
2021-09-30 17:55:14 +0800

    feat: unistring support length, split, substr, substring...

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 // @ts-ignore types
-import regex from 'multichar-regex';
+// import regex from 'multichar-regex';
+
+const RE_IS_UNICODE = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
+const astralRange = /\ud83c[\udffb-\udfff](?=\ud83c[\udffb-\udfff])|(?:[^\ud800-\udfff][\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]?|[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\ud800-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?(?:\u200d(?:[^\ud800-\udfff]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?)*/g;
 
 export default function UniString(str: string): string|UnicodeString {
-  const hasUnicode = regex.test(str);
+  const hasUnicode = RE_IS_UNICODE.test(str);
   if (!hasUnicode) {
     return str;
   }
@@ -17,7 +20,7 @@ export class UniStringClass {
 
   constructor(str: string) {
     this._raw = str;
-    this._hasUnicode = regex.test(str);
+    this._hasUnicode = RE_IS_UNICODE.test(str);
   }
 
   get length(): number {
@@ -41,11 +44,14 @@ export class UniStringClass {
  * Part of Unicode String.
  */
 class UnicodeString {
+  private _raw: string;
   private _matches: string[];
 
   constructor(raw: string) {
+    this._raw = raw;
     // @ts-ignore
-    this._matches = [ ...raw ];
+    // this._matches = [ ...raw ];
+    this._matches = raw.match(astralRange) || [];
   }
 
   get length(): number {
@@ -57,15 +63,16 @@ class UnicodeString {
       return this._matches;
     }
 
-    const sublen = separator.length;
+    const uniSep = separator.match(astralRange);
+    const sublen = uniSep.length;
     const list = [];
     let lastIndex = 0;
-    for (let i = 0, l = this.length; i < l - sublen; i++) {
+    for (let i = 0, l = this.length; i < l; i++) {
       const substr = this._matches.slice(i, i + sublen).join('');
       if (substr === separator) {
         list.push(this._matches.slice(lastIndex, i).join(''));
-        i = i + sublen;
-        lastIndex = i;
+        i = i + sublen - 1; // next for will i++, so here need minus 1.
+        lastIndex = i + 1;
       }
     }
     list.push(this._matches.slice(lastIndex).join(''));
@@ -81,22 +88,49 @@ class UnicodeString {
     if (typeof length === 'number' && length <= 0) {
       return '';
     }
-    let end = typeof length === 'undefined' ? this.length - start : start + length;
+    let end = begin + length;
+    if (typeof length === 'undefined') {
+      end = this.length;
+    }
     return this._matches.slice(begin, end).join('');
   }
 
   substring(start: number, end?: number): string {
-    const begin = start >= 0 ? start : this.length + start;
-    if (begin < 0 || begin > this.length) {
+    let begin = start;
+    if (begin < 0 || isNaN(begin)) {
+      begin = 0;
+    }
+    if (begin > this.length) {
+      begin = this.length;
+    }
+
+    let after = end;
+    if (typeof end === 'undefined') {
+      after = this.length;
+    }
+    if (after < 0 || isNaN(after)) {
+      after = 0;
+    }
+    if (after > this.length) {
+      after = this.length;
+    }
+
+    if (begin === after) {
       return '';
     }
-    let after = this.length;
-    if (typeof end === 'number') {
-      after = end >= 0 ? end : this.length + end;
+
+    if (begin > after) {
+      [begin, after] = [after, begin];
     }
-    if (after < 0 || after > this.length) {
-      return '';
-    }
+
     return this._matches.slice(begin, after).join('');
+  }
+
+  toString() {
+    return this._raw;
+  }
+
+  valueOf() {
+    return this._raw;
   }
 }
---
+++ test/index.test.ts
@@ -0,0 +1,113 @@
+import unistring from '../index';
+
+describe('test/index.test.ts', function() {
+  const enStr = 'abc';
+  const cnStr = '中文汉字';
+  const ceStr = '中文abc';
+  const CNStr = '张𤰉张𤰉';
+  const emojiStr = '张𤰉👨‍👩‍👦和👦🏿很要好';
+
+  const uniEnStr = unistring(enStr);
+  const uniCnStr = unistring(cnStr);
+  const uniCeStr = unistring(ceStr);
+  const uniCNStr = unistring(CNStr);
+  const uniEmojiStr = unistring(emojiStr);
+
+  it('length', function() {
+    expect(enStr.length).toBe(3);
+    expect(uniEnStr.length).toBe(3);
+
+    expect(cnStr.length).toBe(4);
+    expect(uniCnStr.length).toBe(4);
+
+    expect(ceStr.length).toBe(5);
+    expect(uniCeStr.length).toBe(5);
+
+    expect(CNStr.length).toBe(6);
+    expect(uniCNStr.length).toBe(4);
+
+    expect(emojiStr.length).toBe(19);
+    expect(uniEmojiStr.length).toBe(8);
+  });
+
+  it('toString', function() {
+    expect(uniEnStr.toString()).toBe(enStr);
+    expect(uniCnStr.toString()).toBe(cnStr);
+    expect(uniCeStr.toString()).toBe(ceStr);
+    expect(uniCNStr.toString()).toBe(CNStr);
+    expect(uniEmojiStr.toString()).toBe(emojiStr);
+  });
+
+  it('valueOf', function() {
+    expect(uniEnStr.valueOf()).toBe(enStr);
+    expect(uniCnStr.valueOf()).toBe(cnStr);
+    expect(uniCeStr.valueOf()).toBe(ceStr);
+    expect(uniCNStr.valueOf()).toBe(CNStr);
+    expect(uniEmojiStr.valueOf()).toBe(emojiStr);
+  });
+
+  it('split("")', function() {
+    expect(uniEnStr.split('')).toEqual(['a', 'b', 'c']);
+    expect(uniCnStr.split('')).toEqual(['中', '文', '汉', '字']);
+    expect(uniCeStr.split('')).toEqual(['中', '文', 'a', 'b', 'c']);
+    expect(uniCNStr.split('')).toEqual(['张', '𤰉', '张', '𤰉']);
+    expect(uniEmojiStr.split('')).toEqual(['张', '𤰉', '👨‍👩‍👦', '和', '👦🏿', '很', '要', '好']);
+  });
+
+  it('split(char)', function() {
+    expect(uniEnStr.split('b')).toEqual(['a', 'c']);
+    expect(uniCnStr.split('文')).toEqual(['中', '汉字']);
+    expect(uniCeStr.split('文')).toEqual(['中', 'abc']);
+    expect(uniCeStr.split('b')).toEqual(['中文a', 'c']);
+    expect(uniCNStr.split('张')).toEqual(['', '𤰉', '𤰉']);
+    expect(uniCNStr.split('𤰉')).toEqual(['张', '张', '']);
+    expect(uniCNStr.split('张𤰉')).toEqual(['', '', '']);
+    expect(uniEmojiStr.split('𤰉')).toEqual(['张', '👨‍👩‍👦和👦🏿很要好']);
+    expect(uniEmojiStr.split('👦')).toEqual(['张𤰉👨‍👩‍👦和👦🏿很要好']);
+    expect(uniEmojiStr.split('👨‍👩‍👦')).toEqual(['张𤰉', '和👦🏿很要好']);
+  });
+
+  it('substr', function() {
+    expect(uniEnStr.substr(1, 0)).toBe('');
+    expect(uniEnStr.substr(1, 1)).toBe('b');
+    expect(uniCnStr.substr(1, 1)).toBe('文');
+    expect(uniCeStr.substr(1, 1)).toBe('文');
+    expect(uniCeStr.substr(3, 1)).toBe('b');
+
+    expect(uniCNStr.substr(1, 0)).toBe('');
+    expect(uniCNStr.substr(1, -1)).toBe('');
+    expect(uniCNStr.substr(1, 1)).toBe('𤰉');
+    expect(uniCNStr.substr(2, 2)).toBe('张𤰉');
+    expect(uniCNStr.substr(2, -1)).toBe('');
+    expect(uniEmojiStr.substr(1, 2)).toBe('𤰉👨‍👩‍👦');
+    expect(uniEmojiStr.substr(4, 2)).toBe('👦🏿很');
+    expect(uniEmojiStr.substr(4)).toBe('👦🏿很要好');
+    expect(uniEmojiStr.substr(-4)).toBe('👦🏿很要好');
+    expect(uniEmojiStr.substr(-4, 2)).toBe('👦🏿很');
+    expect(uniEmojiStr.substr(-4, -1)).toBe('');
+    expect(uniEmojiStr.substr(-6)).toBe('👨‍👩‍👦和👦🏿很要好');
+  });
+
+  it('substring', function() {
+    expect(uniEnStr.substring(1, 0)).toBe('a');
+    expect(uniEnStr.substring(1, 1)).toBe('');
+    expect(uniEnStr.substring(1, 2)).toBe('b');
+    expect(uniCnStr.substring(1, 2)).toBe('文');
+    expect(uniCeStr.substring(1, 2)).toBe('文');
+    expect(uniCeStr.substring(3, 4)).toBe('b');
+
+    expect(uniCNStr.substring(1, 0)).toBe('张');
+    expect(uniCNStr.substring(1, -1)).toBe('张');
+    expect(uniCNStr.substring(1, 1)).toBe('');
+    expect(uniCNStr.substring(1, 2)).toBe('𤰉');
+    expect(uniCNStr.substring(2, 4)).toBe('张𤰉');
+    expect(uniCNStr.substring(2, -1)).toBe('张𤰉');
+    expect(uniEmojiStr.substring(1, 3)).toBe('𤰉👨‍👩‍👦');
+    expect(uniEmojiStr.substring(4, 6)).toBe('👦🏿很');
+    expect(uniEmojiStr.substring(4)).toBe('👦🏿很要好');
+    expect(uniEmojiStr.substring(-4)).toBe('张𤰉👨‍👩‍👦和👦🏿很要好');
+    expect(uniEmojiStr.substring(-4, 6)).toBe('张𤰉👨‍👩‍👦和👦🏿很');
+    expect(uniEmojiStr.substring(-4, -2)).toBe('');
+    expect(uniEmojiStr.substring(-4, -1)).toBe('');
+  });
+});
